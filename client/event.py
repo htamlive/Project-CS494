4b366d42dd975a05f2c196d628ffe24159681308
Phan Lam Tung <lamtungpro1405@gmail.com>
2024-04-07 01:25:04 +0700

    add event for messasge queue

---
+++ client/event.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from message import Message
+
+
+@dataclass
+class Event:
+    pass
+
+@dataclass
+class UserEnterName(Event):
+    name: str
+
+
+@dataclass
+class UserAnswer(Event):
+    answer: int
+
+
+@dataclass
+class ServerMessage(Event):
+    message: Message
